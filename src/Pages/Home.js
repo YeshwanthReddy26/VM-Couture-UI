ff61d9d62fc74a94148de32172f7345e8b4541e7
Chirra Yeshwanth Reddy <buntyyeshwanth123@gmail.com>
2024-03-13 11:31:57 +0530

    initial commit made

---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <>
+        <h1>
+            Welcome to VM-COUTURE
+        </h1>
+        </>
+
+    );
+}
+
+export default Home;
