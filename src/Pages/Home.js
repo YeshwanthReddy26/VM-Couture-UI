b988f23882f465f14267b678292f961c950dd7d3
YeshwanthReddy26 <buntyyeshwanth123@gmail.com>
2024-03-16 13:55:23 +0530

    Adding constants file

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,15 +2,11 @@ import React, { useState, useEffect } from 'react';
 
 import Box from '@mui/material/Box';
 import Masonry from '@mui/lab/Masonry';
-import img4 from "/Users/tnluser/Desktop/development/VM-Couture-UI/src/assets/HomePagePics/image4.png";
-import img5 from "/Users/tnluser/Desktop/development/VM-Couture-UI/src/assets/HomePagePics/image5.png";
-import img6 from "/Users/tnluser/Desktop/development/VM-Couture-UI/src/assets/HomePagePics/image6.png";
-import img7 from "/Users/tnluser/Desktop/development/VM-Couture-UI/src/assets/HomePagePics/image7.png";
-import img8 from "/Users/tnluser/Desktop/development/VM-Couture-UI/src/assets/HomePagePics/image8.png";
-import img9 from "/Users/tnluser/Desktop/development/VM-Couture-UI/src/assets/HomePagePics/image9.png";
+import { homePageItemsData } from '../Utils/Constants';
 
 const Home = () => {
   const [imagesInRow, setImagesInRow] = useState(3);
+  
   useEffect(() => {
     const handleResize = () => {
       const screenWidth = window.innerWidth;
@@ -28,44 +24,11 @@ const Home = () => {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-    const itemData = [
-        {
-          img: img9,
-          title: 'image 1',
-          text: 'Wrapped in elegance, draped in grace.',
-        },
-        {
-          img: img4,
-          title: 'image 3',
-          text: 'Every fold tells a story.',
-        },
-        {
-          img: img8,
-          title: 'image 4',
-          text: 'Wear with pride',
-        },
-        {
-          img: img5,
-          title: 'image 5',
-          text: 'Saree: Six yards of pure elegance.',
-        },
-        {
-          img: img6,
-          title: 'image 6',
-          text: 'A timeless piece of art',
-        },
-        {
-          img: img7,
-          title: 'image 7',
-          text: 'A symbol of grace and femininity.',
-        },
-      ];
-
     return (
         <>
             <Box sx={{ width: '100%', minHeight: 829}}>
                 <Masonry columns={imagesInRow} spacing={1} sx={{margin: '10px'}}>
-                    {itemData.map((item, index) => (
+                    {homePageItemsData.map((item, index) => (
                         <div class="home-page-image-container" key={index}>
                             <img
                                 className="home-page-image"
---
+++ src/Utils/Constants.js
@@ -0,0 +1,41 @@
+import img4 from "/Users/tnluser/Desktop/development/VM-Couture-UI/src/assets/HomePagePics/image4.png";
+import img5 from "/Users/tnluser/Desktop/development/VM-Couture-UI/src/assets/HomePagePics/image5.png";
+import img6 from "/Users/tnluser/Desktop/development/VM-Couture-UI/src/assets/HomePagePics/image6.png";
+import img7 from "/Users/tnluser/Desktop/development/VM-Couture-UI/src/assets/HomePagePics/image7.png";
+import img8 from "/Users/tnluser/Desktop/development/VM-Couture-UI/src/assets/HomePagePics/image8.png";
+import img9 from "/Users/tnluser/Desktop/development/VM-Couture-UI/src/assets/HomePagePics/image9.png";
+
+const homePageItemsData = [
+    {
+      img: img9,
+      title: 'image 1',
+      text: 'Wrapped in elegance, draped in grace.',
+    },
+    {
+      img: img4,
+      title: 'image 3',
+      text: 'Every fold tells a story.',
+    },
+    {
+      img: img8,
+      title: 'image 4',
+      text: 'Wear with pride',
+    },
+    {
+      img: img5,
+      title: 'image 5',
+      text: 'Saree: Six yards of pure elegance.',
+    },
+    {
+      img: img6,
+      title: 'image 6',
+      text: 'A timeless piece of art',
+    },
+    {
+      img: img7,
+      title: 'image 7',
+      text: 'A symbol of grace and femininity.',
+    },
+  ];
+
+  export {homePageItemsData};
