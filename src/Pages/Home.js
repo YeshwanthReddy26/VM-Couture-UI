7f628cd9d47aca7e3526823b39cd403368cb1a30
YeshwanthReddy26 <buntyyeshwanth123@gmail.com>
2024-03-16 14:14:54 +0530

    Feat: Implement image modal popup on click

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react';
 
 import Box from '@mui/material/Box';
 import Masonry from '@mui/lab/Masonry';
+import Modal from '@mui/material/Modal';
 import { homePageItemsData } from '../Utils/Constants';
 
 const Home = () => {
   const [imagesInRow, setImagesInRow] = useState(3);
-  
+  const [openHomePageImageModal, setOpenHomePageImageModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
   useEffect(() => {
     const handleResize = () => {
       const screenWidth = window.innerWidth;
@@ -24,27 +27,47 @@ const Home = () => {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-    return (
-        <>
-            <Box sx={{ width: '100%', minHeight: 829}}>
-                <Masonry columns={imagesInRow} spacing={1} sx={{margin: '10px'}}>
-                    {homePageItemsData.map((item, index) => (
-                        <div class="home-page-image-container" key={index}>
-                            <img
-                                className="home-page-image"
-                                srcSet={`${item.img}?w=162&auto=format&dpr=2 2x`}
-                                src={`${item.img}?w=162&auto=format`}
-                                alt={item.title}
-                                loading="lazy"
-                            />
-                            {/* <div class="home-page-image-text">{item.text}</div> */}
-                        </div>
-                    ))}
-                </Masonry>
-            </Box>
-        </>
+  const handleImageClick = (imageSrc) => {
+    setSelectedImage(imageSrc);
+    setOpenHomePageImageModal(true);
+  };
 
-    );
+  const handleCloseModal = () => {
+    setOpenHomePageImageModal(false);
+    setSelectedImage(null);
+  };
+
+  return (
+    <>
+      <Box sx={{ width: '100%', minHeight: 829 }}>
+        <Masonry columns={imagesInRow} spacing={1} sx={{ margin: '10px' }}>
+          {homePageItemsData.map((item, index) => (
+            <div className="home-page-image-container" key={index}>
+              <img
+                className="home-page-image"
+                srcSet={`${item.img}?w=162&auto=format&dpr=2 2x`}
+                src={`${item.img}?w=162&auto=format`}
+                alt={item.title}
+                loading="lazy"
+                onClick={() => handleImageClick(item.img)} // Call handleImageClick on image click
+              />
+              {/* <div className="home-page-image-text">{item.text}</div> */}
+            </div>
+          ))}
+        </Masonry>
+      </Box>
+
+      {/* Modal to display the selected image */}
+      <Modal
+        open={openHomePageImageModal}
+        onClose={handleCloseModal}
+      >
+        <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+          <img src={selectedImage} alt="Selected" style={{ maxWidth: '100%', maxHeight: '100%' }} />
+        </Box>
+      </Modal>
+    </>
+  );
 }
 
 export default Home;
