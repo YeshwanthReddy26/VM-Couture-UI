5494a18fe5765e20bb55f436482b0236442e8cc2
YeshwanthReddy26 <97213763+YeshwanthReddy26@users.noreply.github.com>
2024-03-22 11:11:13 +0530

    Feat: Implementation of check delivery status information (#15)
    * Adding delay in displaying review
    
    * Adding delivery charges information

--- src/Components/QualityAssuranceFooter.js
+++ src/Components/QualityAssuranceFooter.js
@@ -5,45 +5,32 @@ import img3 from "/Users/tnluser/Desktop/development/VM-Couture-UI/src/assets/Qu
 import img4 from "/Users/tnluser/Desktop/development/VM-Couture-UI/src/assets/QualityAssuranceImages/empoweringWeavers.png";
 
 function QualityAssuranceFooter() {
-
   return (
     <div class="QualityAssuranceFooterClass">
-      <div>
-        <div>
-          <div class="QualityAssuranceFooterElement QualityAssuranceFooterSlider" style={{paddingTop:'15px'}}>
-            <div class="QualityAssuranceFooterItem QualityAssuranceFooterElement">
-              <div>
-                <div>
-                  <img style={{marginTop:'6px'}} src={img1} alt="icon" loading="lazy" />
-                </div>
-                <h5 class="QualityAssuranceFooterText"> MADE IN INDIA</h5>
-              </div>
-            </div>
-            <div class="QualityAssuranceFooterItem QualityAssuranceFooterElement">
-              <div>
-                <div class="claim-icon">
-                  <img src={img2} alt="icon" loading="lazy" />
-                </div>
-                <h5 style={{marginTop:'10px'}} class="QualityAssuranceFooterText">ASSURED QUALITY</h5>
-              </div>
-            </div>
-            <div class="QualityAssuranceFooterItem QualityAssuranceFooterElement">
-              <div>
-                <div class="claim-icon">
-                  <img src={img3} alt="icon" loading="lazy" />
-                </div>
-                <h5 style={{marginTop:'10px'}} class="QualityAssuranceFooterText">SECURE PAYMENTS</h5>
-              </div>
-            </div>
-            <div class="QualityAssuranceFooterItem QualityAssuranceFooterElement">
-              <div>
-                <div class="claim-icon">
-                  <img style={{marginTop:'9px'}} src={img4} alt="icon" loading="lazy" />
-                </div>
-                <h5 style={{marginTop:'16px'}} class="QualityAssuranceFooterText">EMPOWERING WEAVERS</h5>
-              </div>
-            </div>
+      <div class="QualityAssuranceFooterElement QualityAssuranceFooterSlider" style={{ paddingTop: '15px' }}>
+        <div class="QualityAssuranceFooterItem QualityAssuranceFooterElement">
+          <div>
+            <img style={{ marginTop: '6px' }} src={img1} alt="icon" loading="lazy" />
           </div>
+          <h5 class="QualityAssuranceFooterText"> MADE IN INDIA</h5>
+        </div>
+        <div class="QualityAssuranceFooterItem QualityAssuranceFooterElement">
+          <div class="claim-icon">
+            <img src={img2} alt="icon" loading="lazy" />
+          </div>
+          <h5 style={{ marginTop: '10px' }} class="QualityAssuranceFooterText">ASSURED QUALITY</h5>
+        </div>
+        <div class="QualityAssuranceFooterItem QualityAssuranceFooterElement">
+          <div class="claim-icon">
+            <img src={img3} alt="icon" loading="lazy" />
+          </div>
+          <h5 style={{ marginTop: '10px' }} class="QualityAssuranceFooterText">SECURE PAYMENTS</h5>
+        </div>
+        <div class="QualityAssuranceFooterItem QualityAssuranceFooterElement">
+          <div class="claim-icon">
+            <img style={{ marginTop: '9px' }} src={img4} alt="icon" loading="lazy" />
+          </div>
+          <h5 style={{ marginTop: '16px' }} class="QualityAssuranceFooterText">EMPOWERING WEAVERS</h5>
         </div>
       </div>
     </div>
