69081fc84549976597612b5e41d3069d9787cfa0
YeshwanthReddy26 <buntyyeshwanth123@gmail.com>
2024-03-27 12:42:47 +0530

    Fix: Remove mui components and use div

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,33 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-import Box from '@mui/material/Box';
-import Masonry from '@mui/lab/Masonry';
 import { homePageItemsData } from '../Utils/Constants';
 import { useNavigate } from "react-router-dom";
 import { replaceSpaceWithHyphen } from '../Utils/BaseUtils';
 import '../css/Home.css'
 
 const Home = () => {
-  const [imagesInRow, setImagesInRow] = useState(3);
-
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const handleResize = () => {
-      const screenWidth = window.innerWidth;
-      if (screenWidth < 768) {
-        setImagesInRow(1);
-      } else if (screenWidth < 1024) {
-        setImagesInRow(2);
-      } else {
-        setImagesInRow(3);
-      }
-    };
-
-    window.addEventListener('resize', handleResize);
-    handleResize();
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
 
   const handleHomePageCollectionClick = (item) => {
     navigate(replaceSpaceWithHyphen(item.text));
@@ -35,8 +14,8 @@ const Home = () => {
 
   return (
     <>
-      <Box sx={{ width: '100%', minHeight: 829 }}>
-        <Masonry columns={imagesInRow} spacing={1} sx={{ margin: '10px' }}>
+      <div style={{ width: '100%' }}>
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
           {homePageItemsData.map((item, index) => (
             <div className="home-page-image-container" key={index}>
               <img
@@ -49,8 +28,8 @@ const Home = () => {
               />
             </div>
           ))}
-        </Masonry>
-      </Box>
+        </div>
+      </div>
 
     </>
   );
