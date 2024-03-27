03772c475454ac8049035e26d8a70929b12004e0
YeshwanthReddy26 <buntyyeshwanth123@gmail.com>
2024-03-27 12:49:50 +0530

    Fix: removing MUI Box in About us page

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -15,64 +15,53 @@ import '../css/ContactUs.css'
 const ContactUs = () => {
     return (
         <>
-                <div className="contactUs-page-container">
-                    <div className="contactUs-page-container-item">
-                        <div style={{ textAlign: 'center' }}>
-                            <PlaceIcon style={{ fontSize: '85px' }} />
-                            <p style={{ fontWeight: 'bold', margin: 0, fontSize: '24px', fontFamily: 'Arial, sans-serif' }}>WE ARE LOCATED AT</p>
-                            <p style={{ margin: 0, fontSize: '20px', fontFamily: 'Verdana, sans-serif' }}>Rocktown colony,</p>
-                            <p style={{ margin: 0, fontSize: '20px', fontFamily: 'Verdana, sans-serif' }}>Hyderabad</p>
-                        </div>
-                    </div>
-
-                    <div className="contactUs-page-container-item">
-                        <img src={img1} alt="not loading" style={{ objectFit: 'cover' }} />
-                    </div>
-
-                    <div className="contactUs-page-container-item">
-                        <div style={{ textAlign: 'center' }}>
-                            <WhatsAppIcon style={{ fontSize: '85px' }} />
-                            <p style={{ fontWeight: 'bold', margin: 0, fontSize: '24px', fontFamily: 'Arial, sans-serif' }}>WHATSAPP US</p>
-                            <p style={{ margin: 0, fontSize: '20px', fontFamily: 'Verdana, sans-serif' }}>8688026814</p>
-                            <p style={{ margin: 0, fontSize: '20px', fontFamily: 'Verdana, sans-serif' }}>EMAIL: buntyyeshwanth123@gmail.com</p>
-                        </div>
-                    </div>
-
-                    <div className="contactUs-page-container-item">
-                        <img src={img2} alt="not loading" style={{ objectFit: 'cover' }} />
-                    </div>
-
-                    <div className="contactUs-page-container-item">
-                        <div style={{ textAlign: 'center' }}>
-                            <AccessAlarmIcon style={{ fontSize: '85px' }} />
-                            <p style={{ fontWeight: 'bold', margin: 0, fontSize: '24px', fontFamily: 'Arial, sans-serif' }}>WE ARE AVAILABLE</p>
-                            <p style={{ margin: 0, fontSize: '20px', fontFamily: 'Verdana, sans-serif' }}>Monday-Sunday</p>
-                            <p style={{ margin: 0, fontSize: '20px', fontFamily: 'Verdana, sans-serif' }}>9:30 am - 11:00 pm</p>
-                        </div>
-                    </div>
-
-                    <div className="contactUs-page-container-item">
-                        <img src={img3} alt="not loading" style={{ objectFit: 'cover' }} />
+            <div className="contactUs-page-container">
+                <div className="contactUs-page-container-item">
+                    <div style={{ textAlign: 'center' }}>
+                        <PlaceIcon style={{ fontSize: '85px' }} />
+                        <p style={{ fontWeight: 'bold', margin: 0, fontSize: '24px', fontFamily: 'Arial, sans-serif' }}>WE ARE LOCATED AT</p>
+                        <p style={{ margin: 0, fontSize: '20px', fontFamily: 'Verdana, sans-serif' }}>Rocktown colony,</p>
+                        <p style={{ margin: 0, fontSize: '20px', fontFamily: 'Verdana, sans-serif' }}>Hyderabad</p>
                     </div>
                 </div>
 
-            <Box
-                sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    height: 200,
-                    width: '100%',
-                    backgroundColor: 'black', color: 'white'
-                }}
-            >
-                <div>
-                    <p style={{ fontWeight: 'bold', margin: 0, fontSize: '24px', fontFamily: 'Arial, sans-serif', paddingBottom: '10px' }}>FOLLOW US TO GET THE LATEST UPDATES</p>
-                    <a href="https://www.instagram.com/____iamyesh__/" target="_blank" rel="noopener noreferrer">
-                        <InstagramIcon sx={{ fontSize: 85, color: '#cd486b' }} />
-                    </a>
+                <div className="contactUs-page-container-item">
+                    <img src={img1} alt="not loading" style={{ objectFit: 'cover' }} />
                 </div>
-            </Box>
+
+                <div className="contactUs-page-container-item">
+                    <div style={{ textAlign: 'center' }}>
+                        <WhatsAppIcon style={{ fontSize: '85px' }} />
+                        <p style={{ fontWeight: 'bold', margin: 0, fontSize: '24px', fontFamily: 'Arial, sans-serif' }}>WHATSAPP US</p>
+                        <p style={{ margin: 0, fontSize: '20px', fontFamily: 'Verdana, sans-serif' }}>8688026814</p>
+                        <p style={{ margin: 0, fontSize: '20px', fontFamily: 'Verdana, sans-serif' }}>EMAIL: buntyyeshwanth123@gmail.com</p>
+                    </div>
+                </div>
+
+                <div className="contactUs-page-container-item">
+                    <img src={img2} alt="not loading" style={{ objectFit: 'cover' }} />
+                </div>
+
+                <div className="contactUs-page-container-item">
+                    <div style={{ textAlign: 'center' }}>
+                        <AccessAlarmIcon style={{ fontSize: '85px' }} />
+                        <p style={{ fontWeight: 'bold', margin: 0, fontSize: '24px', fontFamily: 'Arial, sans-serif' }}>WE ARE AVAILABLE</p>
+                        <p style={{ margin: 0, fontSize: '20px', fontFamily: 'Verdana, sans-serif' }}>Monday-Sunday</p>
+                        <p style={{ margin: 0, fontSize: '20px', fontFamily: 'Verdana, sans-serif' }}>9:30 am - 11:00 pm</p>
+                    </div>
+                </div>
+
+                <div className="contactUs-page-container-item">
+                    <img src={img3} alt="not loading" style={{ objectFit: 'cover' }} />
+                </div>
+            </div>
+
+            <div style={{ backgroundColor: 'black', color: 'white', width: '100%' }}>
+                <p style={{ fontWeight: 'bold', margin: 0, fontSize: '24px', fontFamily: 'Arial, sans-serif', paddingBottom: '10px' }}>FOLLOW US TO GET THE LATEST UPDATES</p>
+                <a href="https://www.instagram.com/____iamyesh__/" target="_blank" rel="noopener noreferrer">
+                    <InstagramIcon sx={{ fontSize: 85, color: '#cd486b' }} />
+                </a>
+            </div>
         </>
     );
 }
